a223450fdb965d002f5a42e60adfb99b0733b4b1
yammybajwa <YAMMYBAJWA09@GMAIL.COM>
2022-11-16 12:50:15 +0500

    send api

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+const moongose = require("mongoose");
+
+const cors = require("cors");
+app.use(express.json());
+
+const UserModel = require("./schema/Users.js")
+
+app.use(cors());
+
+moongose.connect("mongodb+srv://admin:admin@cluster0.rpnp6v2.mongodb.net/crud?retryWrites=true&w=majority");
+
+const port =  8000;
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save()
+
+    res.json(user)
+})
+
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port} successfully`)
+})
